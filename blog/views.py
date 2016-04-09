30e91489200a0445bd8175a02501de6f71741f8b
AMontalva <antonio.montalvo.87@gmail.com>
2016-04-09 03:24:33 +0000

    login + migrations

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,14 @@ from .database import session, Entry
 
 from flask import request, redirect, url_for
 
+from flask import flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
+from .database import User
 
+from flask.ext.login import login_required
+
+from flask.ext.login import current_user
 
 @app.route("/", methods=["GET"])
 @app.route("/page/<int:page>", methods=["GET"])
@@ -54,15 +61,18 @@ def entries_post(page=1):
     return redirect(request.base_url + "?limit=" + limit)
 
 @app.route("/entry/add", methods=["GET"])
+@login_required
 def add_entry_get():
     return render_template("add_entry.html")
     
 
 @app.route("/entry/add", methods=["POST"])
+@login_required
 def add_entry_post():
     entry = Entry(
         title=request.form["title"],
         content=request.form["content"],
+        author=current_user
     )
     session.add(entry)
     session.commit()
@@ -107,3 +117,19 @@ def delete_entry_post(id):
 def test():
     # i = request.args.get("var")
     return request.url
+    
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+    
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
